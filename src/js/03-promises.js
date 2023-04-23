57d0cd430debabff85bd0376514d835fecfbec84
ArIonMark1 <andreydatsenko777@gmail.com>
2023-04-23 10:35:11 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,7 +3,6 @@ import Notiflix from 'notiflix';
 const comandInputs = {
   form: document.querySelector('.form'),
 }
-  const promisesArray = [];
 // ######################################################
 comandInputs.form.addEventListener('submit', getFormData);
 // ######################################################
@@ -12,8 +11,12 @@ function getFormData(evt) {
   // 1 після підтвердження форми отримуємо її дані
   evt.preventDefault();
   // ******************
-  const { delay , step, amount } = evt.currentTarget;
-
+  // Активація першого варіанту запуску
+  callPromises(evt.currentTarget);
+  // ******************
+  const { delay, step, amount } = evt.currentTarget
+  
+  // створюємо масив промісів котрим передаємо їх індекс та тривалість виклику
   const promisesArray = new Array(Number(amount.value))
     .fill()
     .map((val, index) => {
@@ -22,23 +25,18 @@ function getFormData(evt) {
       const promiseDelay = Number(delay.value) + index * Number(step.value);
 
       return createPromise(position, promiseDelay);
-      
     });
   
-  console.log(promisesArray) // ok
-
-  Promise.all(promisesArray)
-    .then((x) => console.log('Value from then(): ', x))
-    .catch((err) => console.log(err));
-  // callPromises(promisesArray);
+  // callPromisesFromArr(promisesArray);
 };
-
+// *************************************************
 function createPromise(position, delay) { 
  /*
   2 пишемо функцію яка отримуватиме параметр 'delay' 
   та поверне проміс через вказаний проміжок часу
  */
   return new Promise((resolve, reject) => { 
+
     setTimeout(() => { 
       const shouldResolve = Math.random() > 0.3;
 
@@ -49,17 +47,42 @@ function createPromise(position, delay) {
     }, delay)
   })
 };
+// *************************************************
+// Перший варіант запуску Промісів
+function callPromises(formElement) {
+  // 3 викликаємо функцію яка створює проміси в тій кількості, яку ми задали в полі 'amount'
+  const [delay, step, amount] = formElement;
+  console.log('=> ', delay.value, step.value, amount.value);
 
-// function callPromises(promises) {
-//   // 3 викликаємо функцію яка створює проміси в тій кількості, яку ми задали в полі 'amount'
-//   Promise.all(promises)
-//     .then((val) => console.log('Value from then(): ', val))
-//     .catch((err) => console.log(err));
+  for (let index = 0; index < Number(amount.value); index++) { 
 
-// }
+    const position = index + 1;
+    const promiseDelay = Number(delay.value) + index * Number(step.value);
 
-// 1 після підтвердження форми отримуємо її дані
-// 2 пишемо функцію яка отримуватиме параметр 'delay' та поверне проміс через вказаний проміжок часу
-// 3 викликаємо функцію яка створює проміси в тій кількості, яку ми задали в полі 'amount'
-// 4 при створені промісів кожен наступний проміс створюється через вказаний проміжок часу + заданий крок(step)
-// 5 якщо умова вірна у функції виклику то повертаємо успішний проміс
+    createPromise(position, promiseDelay)
+      .then(({ position, delay }) => {
+
+        Notiflix.Notify.success(`Success: ${position} : ${delay}`);
+        console.log(`Success: ${position} : ${delay}`);
+      })
+      .catch(({ position, delay }) => {
+
+        Notiflix.Notify.failure(`Error!: ${position} : ${delay}`);
+        console.error(`Error!: ${position} : ${delay}`)
+      });
+  };
+
+}
+// Другий варіант запуску Промісів
+function callPromisesFromArr(promiseArr) { 
+  promiseArr.forEach(promise => {
+    promise
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Success: ${position} : ${delay}`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Error!: ${position} : ${delay}`);
+      });
+    
+  });
+}
